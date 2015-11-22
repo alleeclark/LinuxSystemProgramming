84f789a93cfc9f2683807a602a5f9a57d0de8046
alleeclark <allee.clark@bison.howard.edu>
2015-11-22 03:02:38 -0500

    HelloWorld

--- HelloWorld/HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -6,16 +6,9 @@
 void main()
 {
 	int fd;
-	//system call    file write only | create file if doesnt exist, specifies access (rw-r--r--)
+	//system call   file write only | create file if doesnt exist, specifies access (rw-r--r--)
 	fd = open("foo", O_WRONLY | O_CREAT, 0644);
 	write(fd, "hello world", 11);
-	if (fd < 0)
-	{
-		// ernno is only set if an error has occured
-		printf("error number &d\n", errno); 
-		perror("foo");
-		exit(1);
-	}
 	close(fd);
 }
 
