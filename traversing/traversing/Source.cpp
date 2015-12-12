bf3e9e37f2b1623b2cdc76ecf3745ef8abcfda80
alleeclark <allee.clark@bison.howard.edu>
2015-12-12 00:28:30 -0500

    Accessing Command Line Arguments

---
+++ traversing/traversing/Source.cpp
@@ -0,0 +1,10 @@
+// transversing the commandline
+#include <stdio.h>
+
+int main(int argc, char*argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+}
---
+++ traversing/traversing/UsageErrorReporting.cpp
@@ -0,0 +1,13 @@
+// expects exactly one arguments
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		fprint(stderr, "usage: %s file\n", argv[0]);
+		exit(1);
+	}
+	printf("processing %s\n", argv[1]);
+}
